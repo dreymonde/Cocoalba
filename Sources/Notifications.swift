e8bfb8956fa229b54763913aab66c803b322aa2b
Oleg Dreyman <dreymonde@me.com>
2017-01-02 01:37:55 +0200

    - Added UIControlPublisher for UIKit

--- Sources/Notifications.swift
+++ Sources/Notifications.swift
@@ -1,5 +1,8 @@
 import Alba
 import Foundation
+#if os(iOS)
+    import UIKit
+#endif
 
 public extension Notification.Name {
     
@@ -13,3 +16,41 @@ public extension Notification.Name {
     }
         
 }
+
+#if os(iOS)
+
+    internal class UIControlPublisher : NSObject {
+        
+        let publisher = Publisher<Void>()
+        
+        let control: UIControl
+        let controlEvents: UIControlEvents
+        
+        init(control: UIControl, forControlEvents controlEvents: UIControlEvents) {
+            self.control = control
+            self.controlEvents = controlEvents
+        }
+        
+        deinit {
+            control.removeTarget(self, action: #selector(handle), for: controlEvents)
+        }
+        
+        private func register() {
+            control.addTarget(self, action: #selector(handle), for: controlEvents)
+        }
+        
+        func handle() {
+            publisher.publish()
+        }
+        
+    }
+    
+    public extension UIControl {
+        
+        func proxy(forControlEvents controlEvents: UIControlEvents) -> PublisherProxy<Void> {
+            return UIControlPublisher(control: self, forControlEvents: controlEvents).publisher <* PublisherProxy.init(strong:)
+        }
+        
+    }
+    
+#endif
