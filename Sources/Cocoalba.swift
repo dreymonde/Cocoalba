e1d37a5b652fab416a9db05950895f589ca52ce4
Oleg Dreyman <dreymonde@me.com>
2017-01-02 01:28:51 +0200

    - usage function (for README) - Proxy for notifications now also provides (object: Any?)

--- Sources/Cocoalba.swift
+++ Sources/Cocoalba.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 precedencegroup Apply {
     associativity: left
     lowerThan: AdditionPrecedence
@@ -7,4 +9,21 @@ infix operator <* : Apply
 
 func <* <T, V> (lhs: T, rhs: (T) -> V) -> V {
     return rhs(lhs)
+}
+
+func usage() {
+    
+    let operation = Operation()
+    operation.kvoProxy(atKey: "isFinished").listen { (isFinished: Bool) in
+        if isFinished {
+            print("Operation did finish")
+        }
+    }
+
+    Notification.Name.NSThreadWillExit.proxy.listen { (object, userInfo) in
+        if let exitingThread = object as? Thread {
+            print("\(exitingThread) is exiting")
+        }
+    }
+    
 }
--- Sources/Notifications.swift
+++ Sources/Notifications.swift
@@ -3,11 +3,12 @@ import Foundation
 
 public extension Notification.Name {
     
-    var proxy: PublisherProxy<[AnyHashable : Any]?> {
+    var proxy: PublisherProxy<(object: Any?, userInfo: [AnyHashable : Any]?)> {
         return PublisherProxy(subscribe: { (ident, handler) in
             NotificationCenter.default.addObserver(forName: self,
                                                    object: nil,
-                                                   queue: nil) { handler($0.userInfo) }
+                                                   queue: nil) { handler(object: $0.object,
+                                                                         userInfo: $0.userInfo) }
         }, unsubscribe: { _ in })
     }
         
